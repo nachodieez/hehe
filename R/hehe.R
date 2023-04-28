81545067719873c9dda249579012686f598783db
nachodieez <nachodiez10@live.com>
2023-04-28 19:49:31 +0200

    hehehe

--- R/hehe.R
+++ R/hehe.R
@@ -1,17 +1,92 @@
-#' :)
+#' hehehe
 #'
-#' @param url :))
+#' @param x :))
+#' @param y :))
 #' @return :)))
 #' @export
 #' @examples
-#' hehe("https://noproblo.dayjo.org/ZeldaSounds/ST/ST_Fanfare_NewSong.wav")
+#' 1 + 2
 
 
-hehe <- function(url = "https://s3.eu-west-3.amazonaws.com/blob-eu-west-3-qg5chf/sara/cf/cf23/cf23f68e-d2e5-4657-9786-1b8cea3cf0a3.bin?response-content-disposition=attachment%3B%20filename%3D%22hehe.wav%22&response-content-type=audio%2Fx-wav&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA4DM2EY46SOWUMUP4%2F20230427%2Feu-west-3%2Fs3%2Faws4_request&X-Amz-Date=20230427T090727Z&X-Amz-SignedHeaders=host&X-Amz-Expires=1800&X-Amz-Signature=2e986974fd0fbc699836419490d200e10d5cfede954a864525fcfc0876d87dfd"){
-  if (!require("sound")) install.packages("sound")
-  tmp <- tempfile(fileext = ".wav")
-  invisible(download.file(url, tmp, quiet = TRUE))
-  invisible(sound::setWavPlayer(sound::findWavPlayer()[1]))
-  sound::play(tmp)
+`+` <- function(x,y) {
+  r <- runif(1) > 0.1
+  maquinas <- runif(1) < 0.05
+  complete <- runif(1) < 0.1
+  
+  if (maquinas) {
+    print("Lo primero de todo cómo están los máquinas.")
+  }
+  
+  if (r) {
+    if (complete) beepr::beep(4) 
+    sum(x,y)
+  } else {
+    sum(x,y,1)
+  }
 }
 
+
+#' hehehehehehehehe
+#'
+#' @param x :))
+#' @param y :))
+#' @return :)))
+#' @export
+#' @examples
+#' 2 - 1
+
+
+`-` <- function(x,y) {
+  r <- runif(1) > 0.15
+  hehe <- runif(1) < 0.1
+  complete <- runif(1) < 0.1
+  
+  if (hehe) {
+    print("🤠")
+  }
+  
+  if (r) {
+    if (complete) beepr::beep(3) 
+    substract(x,y)
+  } else {
+    x*y
+  }
+}
+
+#' parenthesis
+#'
+#' @param x :))
+#' @return :)))
+#' @export
+
+
+`(` <- function(x) {
+  if (runif(1) < 0.1) {
+    print("Mucho ánimo :))")
+    x
+  } else {
+    x
+  }
+}
+
+#' to establish evil
+#'
+#' @return :)))
+#' @export
+#' 
+
+setup <- function() {
+  if (!require(conflicted)) install.packages("conflicted")
+  if (!require(beepr)) install.packages("beepr")
+  
+  conflicted::conflict_prefer("+", "hehe", "base")
+  conflicted::conflict_prefer("-", "hehe", "base")
+  conflicted::conflict_prefer("(", "hehe", "base")
+  
+}
+
+
+
+substract <- function(x,y) {
+  base::`-`(x,y)
+}
